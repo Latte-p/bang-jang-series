3c77222330975178dd4aa9ee09ce2c66b6fe12bd
Latte-p <woguddldlqslek@gmail.com>
2022-05-24 23:09:14 +0900

    20220523_1

--- python/project/20220524/writedata.py
+++ python/project/20220524/writedata.py
@@ -1,4 +1,4 @@
-f = open("D:/Intellij-project/new/20220524/새파일.txt", 'w')
+f = open("/Users/pjh/Documents/정리하는자료들/개발공부/bang-jang-serise/python/project/20220524/새파일.txt", 'w')
 for i in range(1, 11):
     data = "%d번째 줄입니다.\n" % i
     f.write(data)
