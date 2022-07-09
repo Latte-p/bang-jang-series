6dee9a3f56257859dcee260b265316941cba72ac
Latte-p <woguddldlqslek@gmail.com>
2022-07-09 16:47:48 +0900

    20220709

---
+++ web/exsrc/hello.js
@@ -0,0 +1,18 @@
+function ab(){
+    //alert는 브라우저에 출력되는 알림창 입니다.
+    alert("hello world");
+}
+  
+function soon() {
+  var bangjang = {
+    line: ["support", "mid"],
+    broadcasting_career: "3",
+  };
+  
+  console.log("방장의 주 라인은" + bangjang.line[0] +
+"과" + bangjang.line[1]+"이 되겠습니다. 방송경력은" + 
+bangjang.broadcasting_career + "년 입니다.");
+}
+
+
+
